d08ebd6aef026260681258cad5e810828f833025
vnnie <vinicius.barbosa@aluno.faculdadeimpacta.com.br>
2020-10-09 18:14:33 -0300

    AC3

---
+++ t8.py
@@ -0,0 +1,17 @@
+import os
+from flask import Flask, jsonify, request
+from math import sqrt
+
+app = Flask(__name__)
+
+@app.route('/')
+def nao_entre_em_panico():
+
+
+    primos = "Tudo vai dar certo caros alunos!"
+
+    return primos
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+
